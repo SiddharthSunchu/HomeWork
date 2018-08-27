1badcf3b16eafb97461334b80c77f38b128aa0d0
Siddharth Sunchu <siddharth.sunchu1@gmail.com>
2018-08-27 15:41:36 -0400

    Created React App

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state;
+  }
+  
+
+    render(props) {
+        return (
+            <div className="App">
+                <h1>
+                    Hello
+                </h1>
+            </div>
+        );
+    }
+}
+
+export default App;
